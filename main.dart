1ea395026bda0dc1a7ec57bc8469eb245c0f2a9f
mouli <66820332+mouli7625@users.noreply.github.com>
2021-06-01 10:09:22 +0530

    Scaffold application
    A scaffold with background colour , an App Bar with title , text colour and background colour , a Network Image at the center of the scaffold body

--- main.dart
+++ main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text(
-        'hello world',
-        style: TextStyle(color: Colors.blue),
+    home: Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(child: Image(image: NetworkImage('https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg'),),),
+      appBar: AppBar(
+        title: Text('My first application',
+            style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
       ),
+
     ),
   ));
 }
