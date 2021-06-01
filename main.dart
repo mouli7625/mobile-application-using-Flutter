e033b603de321e76f424ff06118cc4fc53612280
mouli <66820332+mouli7625@users.noreply.github.com>
2021-06-01 23:31:22 +0530

    Flutter Layout
    Use of Hot restart ,Safe Area, Container, Rows and Columns

--- main.dart
+++ main.dart
@@ -1,16 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.lightBlue,
-      body: Center(child: Image(image: NetworkImage('https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg'),),),
-      appBar: AppBar(
-        title: Text('My first application',
-            style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
-      ),
+  runApp(Myapp());
+}
 
-    ),
-  ));
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellowAccent,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.lightGreen,
+                )
+              ],
+            ),
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.blueAccent,
+            )
+          ],
+        ),
+      ),
+    ));
+  }
 }
